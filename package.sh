f95cd5d79b73225202c02ded1bf5f2b8ce661ab3
Ren Tatsumoto <tatsu@autistici.org>
2021-12-12 17:20:59 +0300

    update packaging script

--- package.sh
+++ package.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env sh
 
-cd -- "$(git rev-parse --show-toplevel)" &&
-	git archive HEAD --format=zip -o "RefoldEase_$(git branch --show-current).ankiaddon"
+readonly root_dir=$(git rev-parse --show-toplevel)
+readonly zip_name="RefoldEase_$(git branch --show-current).ankiaddon"
+
+cd -- "$root_dir" || exit 1
+rm -- "$zip_name"
+git archive HEAD --format=zip -o "$zip_name"
+(cd -- ajt_common && git archive HEAD --prefix="${PWD##*/}/" --format=zip -o "$root_dir/${PWD##*/}.zip")
+zipmerge "$zip_name" ./*.zip
+rm -- ./*.zip
