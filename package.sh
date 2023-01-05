f9259ec814e187cb53801294284ee553a3fda80b
Ren Tatsumoto <tatsu@autistici.org>
2023-01-05 03:00:21 +0300

    update package.sh

--- package.sh
+++ package.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 
+readonly addon_name=RefoldEase
 readonly root_dir=$(git rev-parse --show-toplevel)
-readonly zip_name="RefoldEase_$(git branch --show-current).ankiaddon"
+readonly zip_name="${addon_name}_$(git branch --show-current).ankiaddon"
 
 cd -- "$root_dir" || exit 1
-rm -- "$zip_name"
-git archive HEAD --format=zip -o "$zip_name"
+rm -- "$zip_name" 2>/dev/null || true
+
+git archive HEAD --format=zip --output "$zip_name"
 (cd -- ajt_common && git archive HEAD --prefix="${PWD##*/}/" --format=zip -o "$root_dir/${PWD##*/}.zip")
 zipmerge "$zip_name" ./*.zip
 rm -- ./*.zip
